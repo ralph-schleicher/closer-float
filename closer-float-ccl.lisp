1a079da5aefd7fddf2d2fdf0e4ca58137e24c5c5
Ralph Schleicher <rs@ralph-schleicher.de>
2020-03-21 14:02:21 +0000

    New file.
    git-svn-id: https://svn/repo/closer-float/trunk@23 e7eb6e7b-21ee-42d7-bc9a-0698b4583e68

---
+++ closer-float-ccl.lisp
@@ -0,0 +1,93 @@
+;;; closer-float-ccl.lisp --- closer to floating-point arithmetic.
+
+;; Copyright (C) 2020 Ralph Schleicher
+
+;; Redistribution and use in source and binary forms, with or without
+;; modification, are permitted provided that the following conditions
+;; are met:
+;;
+;;    * Redistributions of source code must retain the above copyright
+;;      notice, this list of conditions and the following disclaimer.
+;;
+;;    * Redistributions in binary form must reproduce the above copyright
+;;      notice, this list of conditions and the following disclaimer in
+;;      the documentation and/or other materials provided with the
+;;      distribution.
+;;
+;;    * Neither the name of the copyright holder nor the names of its
+;;      contributors may be used to endorse or promote products derived
+;;      from this software without specific prior written permission.
+;;
+;; THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+;; "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+;; LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+;; FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE
+;; COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+;; INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+;; BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+;; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+;; CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+;; LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+;; ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+;; POSSIBILITY OF SUCH DAMAGE.
+
+;;; Code:
+
+(in-package :closer-float)
+
+;; Positive infinity.
+(defconst short-float-positive-infinity 1S++0)
+(defconst single-float-positive-infinity 1F++0)
+(defconst double-float-positive-infinity 1D++0)
+(defconst long-float-positive-infinity 1L++0)
+
+;; Negative infinity.
+(defconst short-float-negative-infinity -1S++0)
+(defconst single-float-negative-infinity -1F++0)
+(defconst double-float-negative-infinity -1D++0)
+(defconst long-float-negative-infinity -1L++0)
+
+;; Not-a-number.
+(defvar short-float-quiet-not-a-number)
+(defvar single-float-quiet-not-a-number)
+(defvar double-float-quiet-not-a-number)
+(defvar long-float-quiet-not-a-number)
+
+(defvar short-float-signaling-not-a-number)
+(defvar single-float-signaling-not-a-number)
+(defvar double-float-signaling-not-a-number)
+(defvar long-float-signaling-not-a-number)
+
+(defconst short-float-not-a-number 1S+-0)
+(defconst single-float-not-a-number 1F+-0)
+(defconst double-float-not-a-number 1D+-0)
+(defconst long-float-not-a-number 1L+-0)
+
+;; Predicates.
+(defsubst float-positive-infinity-p (x)
+  (declare (type float x))
+  (etypecase x
+    (single-float
+     (= x single-float-positive-infinity))
+    (double-float
+     (= x double-float-positive-infinity))))
+
+(defsubst float-negative-infinity-p (x)
+  (declare (type float x))
+  (etypecase x
+    (single-float
+     (= x single-float-negative-infinity))
+    (double-float
+     (= x double-float-negative-infinity))))
+
+(defsubst float-infinity-p (x)
+  (declare (type float x))
+  (or (float-positive-infinity-p x)
+      (float-negative-infinity-p x)))
+
+(defsubst float-not-a-number-p (x)
+  (declare (type float x))
+  (and (ccl::nan-or-infinity-p x)
+       (not (float-infinity-p x))))
+
+;;; closer-float-ccl.lisp ends here
